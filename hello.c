4c905be8ff9703f274fd9493b9dd0bf3dd7e0cf5
Julio Silva <jsilvafranco2@huskers.unl.edu>
2022-08-25 21:12:27 +0000

    added major to the output

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Julio Silva!
+ *         EMAIL
+ * Date: 2022/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+printf("julio Silva!!\n");
+printf("Electrical Engineer!/n");
+  return 0;
+}
+
